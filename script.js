a4a87ab992375d382c2e49058e3bf97dc0155773
Ge0rgicus <sanjaadorjenhbaatar555@gmail.com>
2026-04-15 19:15:30 +1000

    Trying

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const result = document.getElementById("result");
 
 async function loadModels() {
   await faceapi.nets.ssdMobilenetv1.loadFromUri("./models");
+  await faceapi.nets.faceLandmark68Net.loadFromUri("./models");
+  await faceapi.nets.faceRecognitionNet.loadFromUri("./models");
   await faceapi.nets.ageGenderNet.loadFromUri("./models");
 }
-
 
 loadModels();
 
@@ -17,7 +18,11 @@ upload.addEventListener("change", async () => {
   result.textContent = "Detecting age...";
 
   const img = await faceapi.bufferToImage(file);
-  const detection = await faceapi.detectSingleFace(img).withAgeAndGender();
+
+  const detection = await faceapi
+    .detectSingleFace(img)
+    .withFaceLandmarks()
+    .withAgeAndGender();
 
   if (!detection) {
     result.textContent = "No face detected.";
